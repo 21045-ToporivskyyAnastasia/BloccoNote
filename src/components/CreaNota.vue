0a3207020d868a78d537e5fd9511aef8574664b7
Anastasia <122974402+21045-ToporivskyyAnastasia@users.noreply.github.com>
2023-06-14 15:23:13 +0200

    asdfghjk

--- src/components/CreaNota.vue
+++ src/components/CreaNota.vue
@@ -1,113 +1,113 @@
 <!-- Template del componente create-note-modal -->
 <template id="create-note-modal-template">
-    <div class="modal">
-      <div class="modal-content">
-        <form @submit.prevent="onSubmit" class="form-group">
-          <textarea id="newTitle" v-model="newTitle" placeholder="Inserisci titolo" maxlength="100" required></textarea>
-          <br>
-          <textarea id="newContent" v-model="newContent" placeholder="Inserisci testo" maxlength="800" required></textarea>
-          <br>
-          
-          <div class="modal-buttons">
-            <button id="submitButton" type="submit" >Salva</button>
-            <button id="cancelButton" @click="cancel" >Annulla</button>
-          </div>
-        </form>
-      </div>
+  <div class="modal">
+    <div class="modal-content">
+      <form @submit.prevent="onSubmit" class="form-group">
+        <textarea id="newTitle" v-model="newTitle" placeholder="Inserisci titolo" maxlength="100" required></textarea>
+        <br>
+        <textarea id="newContent" v-model="newContent" placeholder="Inserisci testo" maxlength="800" required></textarea>
+        <br>
+        
+        <div class="modal-buttons">
+          <button id="submitButton" type="submit" >Salva</button>
+          <button id="cancelButton" @click="cancel" >Annulla</button>
+        </div>
+      </form>
     </div>
-  </template>
+  </div>
+</template>
 
-  
-  <script>
-  export default {
-    name: "CreaNota",
-    data() {
-        return {
-          newTitle: '',
-          newContent: '',
-          newDate: '',
-        };
+
+<script>
+export default {
+  name: "CreaNota",
+  data() {
+      return {
+        newTitle: '',
+        newContent: '',
+        newDate: '',
+      };
+  },
+  methods: {
+    onSubmit() {
+      // Logica per aggiungere una nota
+      this.$emit('add-note', {
+        id: Date.now(),
+        title: this.newTitle.trim(),
+        content: this.newContent.trim(),
+        date: new Date(),
+      });
+      this.newTitle = '';
+      this.newContent = '';
     },
-    methods: {
-      onSubmit() {
-        // Logica per aggiungere una nota
-        this.$emit('add-note', {
-          id: Date.now(),
-          title: this.newTitle.trim(),
-          content: this.newContent.trim(),
-          date: new Date(),
-        });
-        this.newTitle = '';
-        this.newContent = '';
-      },
-      cancel() {
-        // Logica per annullare la creazione di una nota
-        this.$emit('cancel');
-      },
-
+    cancel() {
+      // Logica per annullare la creazione di una nota
+      this.$emit('cancel');
     },
-  }
-  </script>
-  <!--design del pop-up crea nota con css-->
-  <style scoped>
-  * {
-    color: black;
-  }
-  textarea {
-    font-size: 24px;
-  }
-  #newContent {
-    height: 350px;
-    width: 605px;
-    background-color: rgb(66, 76, 87);
-    color: white;
-    resize: none;
-  }
-  #newTitle {
-    height: 50px;
-    width: 605px;
-    background-color: rgb(66, 76, 87);
-    color: white;
-    resize: none;
-  }
-  .modal {
-    width: 650px;
-    background-color: rgb(66, 76, 87);
-    padding: 30px;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 3%;
-  }
 
-  .modal-buttons {
-    padding: 10px 20px;
-    font-size: 30px;
-    width: 100%;
-    color: white;
-    margin-top: 10px;
-  }
-  #submitButton {
-    border: 1px solid rgb(66, 76, 87);
-    background-color: rgb(27, 157, 217);
-    cursor: pointer;
-    width: 49%;
-    height: 40px;
-    color: black;
-    font-size: 18px;
-    margin-right: 2%;
-  }
-  #cancelButton {
-    border: 1px solid rgb(66, 76, 87);
-    background-color: rgb(217, 217, 217);
-    cursor: pointer;
-    height: 40px;
-    width: 49%;
-    color: black;
-    font-size: 18px;
-  }
+  },
+}
+</script>
+<!--design del pop-up crea nota con css-->
+<style scoped>
+* {
+  color: black;
+}
+textarea {
+  font-size: 24px;
+}
+#newContent {
+  height: 350px;
+  width: 605px;
+  background-color: rgb(66, 76, 87);
+  color: white;
+  resize: none;
+}
+#newTitle {
+  height: 50px;
+  width: 605px;
+  background-color: rgb(66, 76, 87);
+  color: white;
+  resize: none;
+}
+.modal {
+  width: 650px;
+  background-color: rgb(66, 76, 87);
+  padding: 30px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3%;
+}
+
+.modal-buttons {
+  padding: 10px 20px;
+  font-size: 30px;
+  width: 100%;
+  color: white;
+  margin-top: 10px;
+}
+#submitButton {
+  border: 1px solid rgb(66, 76, 87);
+  background-color: rgb(27, 157, 217);
+  cursor: pointer;
+  width: 49%;
+  height: 40px;
+  color: black;
+  font-size: 18px;
+  margin-right: 2%;
+}
+#cancelButton {
+  border: 1px solid rgb(66, 76, 87);
+  background-color: rgb(217, 217, 217);
+  cursor: pointer;
+  height: 40px;
+  width: 49%;
+  color: black;
+  font-size: 18px;
+}
 </style>
--- src/components/ListaNote.vue
+++ src/components/ListaNote.vue
@@ -1,125 +1,125 @@
 <!-- Template del componente note-list -->
 <template id="note-list-template">
-    <div id="container">
-      <ul class="note-list">
-      <li v-for="note in notes" :key="note.id" class="note-item" >
-        <button class="remove-button" @click="confirmRemove(note)">x</button>
-        <h3 class="note-title">{{ note.title }}</h3>
-        <p class="note-content" @click="showModificaNota(note)">{{ truncateText(note.content, 200) }}</p>
-        <p class="note-date" @click="showModificaNota(note)">Data: {{ note.date }} {{ note.operatorName }} {{ note.operatorSurname }}</p>
-      </li>
-    </ul>
-    <button id="addNote" @click="showAddNote" >+</button>
-  </div>
-  </template>
-  
-  <script>
-  export default{
-    name: "ListaNote",
-    props: ['notes'],
-    methods: {
-        //Logica per modificare una nota schiacciando il pulsante "modifica"
-        showModificaNota(note){
-            this.$emit('modifica-nota', note)
-          },
-        //Logica per modificare una nota schiacciando il pulsante "+"
-        showAddNote(){
-          this.$emit('add-note');
+  <div id="container">
+    <ul class="note-list">
+    <li v-for="note in notes" :key="note.id" class="note-item" >
+      <button class="remove-button" @click="confirmRemove(note)">x</button>
+      <h3 class="note-title">{{ note.title }}</h3>
+      <p class="note-content" @click="showModificaNota(note)">{{ truncateText(note.content, 200) }}</p>
+      <p class="note-date" @click="showModificaNota(note)">Data: {{ note.date }} {{ note.operatorName }} {{ note.operatorSurname }}</p>
+    </li>
+  </ul>
+  <button id="addNote" @click="showAddNote" >+</button>
+</div>
+</template>
+
+<script>
+export default{
+  name: "ListaNote",
+  props: ['notes'],
+  methods: {
+      //Logica per modificare una nota schiacciando il pulsante "modifica"
+      showModificaNota(note){
+          this.$emit('modifica-nota', note)
         },
-        //Logica per fare uscire il pop-up di conferma rimozione schiacciando il tasto "x"
-        confirmRemove(note) {
-          this.$emit('confirm-remove', note.id);
-        },
-        //metodo per per limitare la visualizzazione dei caratteri nella schermata principale
-        truncateText(text, limit) {
-          if (text.length > limit) {
-            return text.slice(0, limit) + '...';
-          } else {
-            return text;
-          }
-              },
-            },  
-      };
-  
-  </script>
-  <!--design della schermata principale con tutte le note-->
+      //Logica per modificare una nota schiacciando il pulsante "+"
+      showAddNote(){
+        this.$emit('add-note');
+      },
+      //Logica per fare uscire il pop-up di conferma rimozione schiacciando il tasto "x"
+      confirmRemove(id) {
+        this.$emit('confirm-remove', id);
+      },
+      //metodo per per limitare la visualizzazione dei caratteri nella schermata principale
+      truncateText(text, limit) {
+        if (text.length > limit) {
+          return text.slice(0, limit) + '...';
+        } else {
+          return text;
+        }
+            },
+          },  
+    };
+
+</script>
+<!--design della schermata principale con tutte le note-->
 <style scoped>
-  * {
-    color: black;
-  }
-  #container {
-    justify-content: center;  
-  }
-  .note-list {
-    margin-top: 0;
-    background-color: white;
-    padding-top: 150px;
-    padding-left: 0; 
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .remove-button {
-    background-color: rgb(27, 157, 217);
-    height: 40px;
-    width: 40px;
-    border: none;
-    font-size: 20px;
-    position: absolute;
-    right: 9%;
-    cursor: pointer;
-  }
-  .note-item {
-    width: 82%;
-    height: auto;
-    background-color: rgb(217, 217, 217);
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-bottom: 30px;
-    border: 1px solid rgb(66, 76, 87);
-    cursor: pointer;
-  }
-  h3 {
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 5px;
-    padding-bottom: 2px;
-    word-wrap: break-word;
-    background-color: rgb(66, 76, 87);
-    color: white;
-    width: 100%;
-    text-align: center;
-    justify-content: space-between;
-    padding-right: 45px;
-  }
-  .note-content {
-    font-size: 16px;
-    white-space: pre-wrap;
-    word-wrap: break-word;
-    padding-left: 2%;
-    padding-right: 2%;
-  }
-  .note-date {
-    font-size: 12.5px;
-    margin-top: 8px;
-    font-weight: bold;
-    padding-left: 2%;
-  }
-  #addNote {
-    border: 1.9px solid rgb(0, 0, 0);
-    padding: 12px;
-    width: 80px;
-    height: 80px;
-    cursor: pointer;
-    border-radius: 391045604672569465265475098295863276965709465072965246502965606253046502574857948750284504650647694584909257205627603264954092369024652056039465036757px;
-    color: rgb(0, 0, 0);
-    border-color:black;
-    font-size: 50px;
-    position: fixed;
-    right: 30px;
-    bottom: 30px;
-    background-color: rgb(27, 157, 217);
-  }
+* {
+  color: black;
+}
+#container {
+  justify-content: center;  
+}
+.note-list {
+  margin-top: 0;
+  background-color: white;
+  padding-top: 150px;
+  padding-left: 0; 
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.remove-button {
+  background-color: rgb(27, 157, 217);
+  height: 40px;
+  width: 40px;
+  border: none;
+  font-size: 20px;
+  position: absolute;
+  right: 9%;
+  cursor: pointer;
+}
+.note-item {
+  width: 82%;
+  height: auto;
+  background-color: rgb(217, 217, 217);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  border: 1px solid rgb(66, 76, 87);
+  cursor: pointer;
+}
+h3 {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 5px;
+  padding-bottom: 2px;
+  word-wrap: break-word;
+  background-color: rgb(66, 76, 87);
+  color: white;
+  width: 100%;
+  text-align: center;
+  justify-content: space-between;
+  padding-right: 45px;
+}
+.note-content {
+  font-size: 16px;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  padding-left: 2%;
+  padding-right: 2%;
+}
+.note-date {
+  font-size: 12.5px;
+  margin-top: 8px;
+  font-weight: bold;
+  padding-left: 2%;
+}
+#addNote {
+  border: 1.9px solid rgb(0, 0, 0);
+  padding: 12px;
+  width: 80px;
+  height: 80px;
+  cursor: pointer;
+  border-radius: 391045604672569465265475098295863276965709465072965246502965606253046502574857948750284504650647694584909257205627603264954092369024652056039465036757px;
+  color: rgb(0, 0, 0);
+  border-color:black;
+  font-size: 50px;
+  position: fixed;
+  right: 30px;
+  bottom: 30px;
+  background-color: rgb(27, 157, 217);
+}
 </style>
